a6368c6993cb1423fa022881ec1a4de996861fd8
iamcaye <cayetano.biehler@gmail.com>
2021-10-11 18:45:50 +0200

    adding link to mailing

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -4,13 +4,17 @@ import Caye from '../img/caye-hi.png'
 
 export const Contact = () => {
   return (
-    <>
-    <img src={Caye} alt='caye' className='caye'/>
-    <h1>Contact</h1>
-    <a className='iglink' target='noopener' href='https://www.instagram.com/cayetanobiehler/'>Instagram : @cayetanobiehler</a>
-    <p>Gmail : cayetano.biehler@gmail.com</p>
-    <a className='iglink' target='noopener' href='https://www.linkedin.com/in/cayetano-biehler-pollaci-0074091b0/'>LinkedIn: Cayetano Biehler</a>
-    </>
+    <div className='contactDiv'>
+      <img src={Caye} alt='caye' className='caye'/>
+      <h1>Contact</h1>
+      <a className='iglink' target='noopener' href='https://www.instagram.com/cayetanobiehler/'>Instagram : @cayetanobiehler</a>
+      <br/>
+      <br/>
+      <a className='iglink' target='noopener' href='mailto:cayetano.biehler@gmail.com'>Gmail : cayetano.biehler@gmail.com</a>
+      <br/>
+      <br/>
+      <a className='iglink' target='noopener' href='https://www.linkedin.com/in/cayetano-biehler-pollaci-0074091b0/'>LinkedIn: Cayetano Biehler</a>
+    </div>
   );
 }
 
