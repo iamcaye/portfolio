0c331e0315289b4db4ac06471b7e4d9507e43a57
iamcaye <cayetano.biehler@gmail.com>
2021-10-11 21:22:56 +0200

    Updates

---
+++ precache-manifest.e938167d42eb257ee8dc7a23730d2ec5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "806e67eda68ca8679bdde21e4be30fc0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "50f4732e44dfd4d4151d",
+    "url": "/portfolio/static/css/main.85ef27fa.chunk.css"
+  },
+  {
+    "revision": "e6474a03e34cb749b90a",
+    "url": "/portfolio/static/js/2.c458ac5f.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/portfolio/static/js/2.c458ac5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50f4732e44dfd4d4151d",
+    "url": "/portfolio/static/js/main.997bf8d1.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "b7b7076dc842ac6ac3ddf7ce3e99f008",
+    "url": "/portfolio/static/media/caye-hi.b7b7076d.png"
+  },
+  {
+    "revision": "22373edef43e7205f3f578aff78d7b1f",
+    "url": "/portfolio/static/media/caye-pc.22373ede.png"
+  },
+  {
+    "revision": "364ac79246037d4840273d634f056f18",
+    "url": "/portfolio/static/media/caye.364ac792.png"
+  }
+]);
