b1d5e4e456425c674e9a7022c2a17eb726e7dc1b
iamcaye <cayetano.biehler@gmail.es>
2021-11-24 23:39:49 +0100

    br

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -8,6 +8,8 @@ export const Contact = () => {
       <img src={Caye} alt='caye' className='caye'/>
       <h1>Contact</h1>
       <a className='iglink' target='noopener' href='https://github.com/iamcaye'>Github : @iamcaye</a>
+      <br/>
+      <br/>
       <a className='iglink' target='noopener' href='https://www.instagram.com/cayetanobiehler/'>Instagram : @cayetanobiehler</a>
       <br/>
       <br/>
