9da45339e3f500df87216fa5581075bd01a62d7d
iamcaye <cayetano.biehler@gmail.com>
2023-05-28 14:30:46 +0200

    change url and icon

---
+++ build/precache-manifest.f3d1a5402d2576056e651d6a99bbf5df.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff375f3f6f9240d6362148707ec230c4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b8a06e023a8908351c57",
+    "url": "/portfolio/static/css/main.f6b7e454.chunk.css"
+  },
+  {
+    "revision": "febc407fe16a03fc96e6",
+    "url": "/portfolio/static/js/2.f0181f13.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/portfolio/static/js/2.f0181f13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8a06e023a8908351c57",
+    "url": "/portfolio/static/js/main.a4a602ce.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "b7b7076dc842ac6ac3ddf7ce3e99f008",
+    "url": "/portfolio/static/media/caye-hi.b7b7076d.png"
+  },
+  {
+    "revision": "22373edef43e7205f3f578aff78d7b1f",
+    "url": "/portfolio/static/media/caye-pc.22373ede.png"
+  },
+  {
+    "revision": "364ac79246037d4840273d634f056f18",
+    "url": "/portfolio/static/media/caye.364ac792.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.dcfafb1d14650839aa0c140ac478d391.js"
+  "/portfolio/precache-manifest.f3d1a5402d2576056e651d6a99bbf5df.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
